8846d37ca384fa6092627e5a134e5831012a265b
CodedAbyss <46334364+CodedAbyss@users.noreply.github.com>
2025-06-06 15:33:00 -0400

    Update interface.h

--- interface.h
+++ interface.h
@@ -1,67 +1,197 @@
+#ifndef INTERFACE_INCLUDED
+#define INTERFACE_INCLUDED
 
-#define nums() 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
-#define args_n(_1,_2,_3,_4,_5,_6,_7,_8,_9,_10,_11,_12,N,...) N
-#define nargs(...) expand(args_n, __VA_ARGS__, nums())
-#define m1args(v, ...) nargs(__VA_ARGS__)
-#define expand(fn, ...) fn(__VA_ARGS__)
-#define append(a, b) a##b
+#define APPEND(a, b) a##b
 
-#define decom8(a,...) a decom7(__VA_ARGS__)
-#define decom7(a,...) a decom6(__VA_ARGS__)
-#define decom6(a,...) a decom5(__VA_ARGS__)
-#define decom5(a,...) a decom4(__VA_ARGS__)
-#define decom4(a,...) a decom3(__VA_ARGS__)
-#define decom3(a,...) a decom2(__VA_ARGS__)
-#define decom2(a,...) a decom1(__VA_ARGS__)
-#define decom1(a,...) a
+// Number of args - expands to the number of arguments
+#define NARGS(...) EXPAND(ARGS_N, __VA_ARGS__, NUMS())
+#define NUMS() 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
+#define ARGS_N(_1,_2,_3,_4,_5,_6,_7,_8,_9,_10,_11,_12,N,...) N
 
-#define apply8(f, c, a, ...) f(c, a), apply7(f, c, __VA_ARGS__)
-#define apply7(f, c, a, ...) f(c, a), apply6(f, c, __VA_ARGS__)
-#define apply6(f, c, a, ...) f(c, a), apply5(f, c, __VA_ARGS__)
-#define apply5(f, c, a, ...) f(c, a), apply4(f, c, __VA_ARGS__)
-#define apply4(f, c, a, ...) f(c, a), apply3(f, c, __VA_ARGS__)
-#define apply3(f, c, a, ...) f(c, a), apply2(f, c, __VA_ARGS__)
-#define apply2(f, c, a, ...) f(c, a), apply1(f, c, __VA_ARGS__)
-#define apply1(f, c, a, ...) f(c, a)
-#define APPLY_ALL(fn, ctx, ...) expand(expand(append, apply, nargs(__VA_ARGS__)), fn, ctx, __VA_ARGS__)
-#define APPLY_ALLm1(fn, ctx, ...) expand(expand(append, apply, m1args(__VA_ARGS__)), fn, ctx, __VA_ARGS__)
-#define decom(...) expand(expand(append, decom, nargs(__VA_ARGS__)), __VA_ARGS__)
+// Minus 1 args - gets the number of arguments minus one
+#define M1ARGS(v, ...) NARGS(__VA_ARGS__)
 
-#define get_name(ret, name, ...) name
-#define keep(n) n
-#define toss(n)
-#define keep_type(ctx, var) keep var
-#define extract_var(ctx, var) toss var
+// The C preprocessor is not particularly smart.
+// It won't expand recursively. As such we need multiple identical macros so we can nest expansions.
+// This is why we need two APPLY_ALL macros, and three EXPAND macros, because otherwise the macro won't fully expand.
 
-#define INTERFACE_DECLARE(ret, name, ...) ret (*name)(APPLY_ALL(keep_type, 0, __VA_ARGS__));
+// Applies fn(ctx, arg) for all arguments
+#define APPLY_ALL(fn, ctx, ...) EXPAND(EXPAND(APPEND, APPLY, NARGS(__VA_ARGS__)), fn, ctx, __VA_ARGS__)
+#define APPLY_ALL2(fn, ctx, ...) EXPAND2(EXPAND2(APPEND, APPLY_, NARGS(__VA_ARGS__)), fn, ctx, __VA_ARGS__)
+
+// Applies fn(ctx, arg) for all arguments but the last one
+#define APPLY_ALLm1(fn, ctx, ...) EXPAND(EXPAND(APPEND, APPLY, M1ARGS(__VA_ARGS__)), fn, ctx, __VA_ARGS__)
+
+// Removes the commas between all arguments
+#define DECOM(...) EXPAND(EXPAND(APPEND, DECOM, NARGS(__VA_ARGS__)), __VA_ARGS__)
+
+// Applies the function to the args. This is used a lot as it forces the preprocesser to expand the arguments
+#define EXPAND(fn, ...)  fn(__VA_ARGS__)
+#define EXPAND2(fn, ...) fn(__VA_ARGS__)
+#define EXPAND3(fn, ...) fn(__VA_ARGS__)
+
+#define APPLY_12(f, c, a, ...) f(c, a), APPLY_11(f, c, __VA_ARGS__)
+#define APPLY_11(f, c, a, ...) f(c, a), APPLY_10(f, c, __VA_ARGS__)
+#define APPLY_10(f, c, a, ...) f(c, a), APPLY_9(f, c, __VA_ARGS__)
+#define APPLY_9(f, c, a, ...) f(c, a), APPLY_8(f, c, __VA_ARGS__)
+#define APPLY_8(f, c, a, ...) f(c, a), APPLY_7(f, c, __VA_ARGS__)
+#define APPLY_7(f, c, a, ...) f(c, a), APPLY_6(f, c, __VA_ARGS__)
+#define APPLY_6(f, c, a, ...) f(c, a), APPLY_5(f, c, __VA_ARGS__)
+#define APPLY_5(f, c, a, ...) f(c, a), APPLY_4(f, c, __VA_ARGS__)
+#define APPLY_4(f, c, a, ...) f(c, a), APPLY_3(f, c, __VA_ARGS__)
+#define APPLY_3(f, c, a, ...) f(c, a), APPLY_2(f, c, __VA_ARGS__)
+#define APPLY_2(f, c, a, ...) f(c, a), APPLY_1(f, c, __VA_ARGS__)
+#define APPLY_1(f, c, a, ...) f(c, a)
+#define APPLY_0(...)
+
+#define APPLY12(f, c, a, ...) f(c, a), APPLY11(f, c, __VA_ARGS__)
+#define APPLY11(f, c, a, ...) f(c, a), APPLY10(f, c, __VA_ARGS__)
+#define APPLY10(f, c, a, ...) f(c, a), APPLY9(f, c, __VA_ARGS__)
+#define APPLY9(f, c, a, ...) f(c, a), APPLY8(f, c, __VA_ARGS__)
+#define APPLY8(f, c, a, ...) f(c, a), APPLY7(f, c, __VA_ARGS__)
+#define APPLY7(f, c, a, ...) f(c, a), APPLY6(f, c, __VA_ARGS__)
+#define APPLY6(f, c, a, ...) f(c, a), APPLY5(f, c, __VA_ARGS__)
+#define APPLY5(f, c, a, ...) f(c, a), APPLY4(f, c, __VA_ARGS__)
+#define APPLY4(f, c, a, ...) f(c, a), APPLY3(f, c, __VA_ARGS__)
+#define APPLY3(f, c, a, ...) f(c, a), APPLY2(f, c, __VA_ARGS__)
+#define APPLY2(f, c, a, ...) f(c, a), APPLY1(f, c, __VA_ARGS__)
+#define APPLY1(f, c, a, ...) f(c, a)
+#define APPLY0(...)
+
+#define DECOM12(a,...) a DECOM11(__VA_ARGS__)
+#define DECOM11(a,...) a DECOM10(__VA_ARGS__)
+#define DECOM10(a,...) a DECOM9(__VA_ARGS__)
+#define DECOM9(a,...) a DECOM8(__VA_ARGS__)
+#define DECOM8(a,...) a DECOM7(__VA_ARGS__)
+#define DECOM7(a,...) a DECOM6(__VA_ARGS__)
+#define DECOM6(a,...) a DECOM5(__VA_ARGS__)
+#define DECOM5(a,...) a DECOM4(__VA_ARGS__)
+#define DECOM4(a,...) a DECOM3(__VA_ARGS__)
+#define DECOM3(a,...) a DECOM2(__VA_ARGS__)
+#define DECOM2(a,...) a DECOM1(__VA_ARGS__)
+#define DECOM1(a,...) a
+#define DECOM0(a,...) a
+
+// this may look pointless, but it's quite useful.
+// we store the arguments like "(type) var_name"
+// KEEP (type) var_name => type var_name
+// TOSS (type) var_name => var_name
+#define KEEP(n) n
+#define TOSS(n)
+#define KEEP_TYPE(ctx, var) KEEP var
+#define EXTRACT_VAR(ctx, var) TOSS var
+
+#define GEN_IFACE_VTBL(ret, name, ...) ret (*name)(APPLY_ALL(KEEP_TYPE, 0, __VA_ARGS__));
 #define GEN_ENUM(ctx, var) TYPE##_##ctx##_##var
-#define iface(name, ...) \
+#define GEN_TYPEDEFS(ctx, var) typedef struct var var;
+
+#define COPY(...) __VA_ARGS__
+#define JOIN_ARGS(ctx, arg) (ctx, COPY arg)
+
+#define GEN_IFACE_FUNC_HELPER(...) (APPLY_ALL(KEEP_TYPE, 0, __VA_ARGS__)), (APPLY_ALL(EXTRACT_VAR, 0, __VA_ARGS__))
+#define GEN_IFACE_FUNC_INFO(ret, name, ...) (ret, name, TOSS DECOM1(__VA_ARGS__), GEN_IFACE_FUNC_HELPER(__VA_ARGS__)),
+#define GEN_IFACE_FUNC_ENTRY(iface, ret, name, self, params, vars) inline ret iface##_##name params { return iface##_##vtbls[self->type_id].name vars; }
+#define GEN_IFACE_FUNC(ctx, arg) GEN_IFACE_FUNC_ENTRY JOIN_ARGS(ctx, arg)
+
+#define GEN_VTBL_NAME(ret, name, ...) name,
+#define GEN_VTBL_ENTRY(ctx, var) .var = (void*)ctx##_##var
+#define GEN_VTBLS(iface, name) [TYPE_##iface##_##name] = { APPLY_ALLm1(GEN_VTBL_ENTRY, name, iface##_IFACE(GEN_VTBL_NAME) 0) }
+
+#define GET_FUNC_DEC_INFO(ret, name, self, ...) (ret, name, TOSS self, APPLY_ALL(KEEP_TYPE, 0, 0, __VA_ARGS__)),
+#define GEN_FUNC_DEC_HELPER(type, ret, fn, self, discard, ...) ret type##_##fn (type *self, ##__VA_ARGS__);
+#define GEN_FUNC_DEC_ENTRY(ctx, var) GEN_FUNC_DEC_HELPER JOIN_ARGS(ctx, var)
+#define GEN_FUNC_DEC(iface, name) APPLY_ALLm1(GEN_FUNC_DEC_ENTRY, name, iface##_IFACE(GET_FUNC_DEC_INFO) 0)
+
+// The final interface macro!
+// USAGE:
+// Step 1. define the interface
+// #define shape_IFACE(fn) \
+//     fn(float, area, (shape*) self) \
+//     fn(void, scale, (shape*) self, float factor)
+// 
+// Step 2. define its subtypes
+// interface(shape, rectangle, triangle, circle);
+//
+// Step 3. implement your subtypes
+//
+// typedef struct rectangle { shape parent; float x, y, w, h; } rectangle;
+// typedef struct triangle { shape parent; float x[3], y[3]; } triangle;
+// typedef struct circle { shape parent; float x, y, radius; } circle;
+//
+// int rectangle_area(rectangle *self) { return self->w * self->h; }
+// void rectangle_scale(rectangle *self, float factor) {
+//     self->w *= factor;
+//     self->h *= factor;
+//     self->x -= self->w / factor;
+//     self->h -= self->h / factor;
+// }
+// float triangle_area(triangle *t) {
+//     return 0.5f * (
+//         t->x[0] * (t->y[1] - t->y[2]) + 
+//         t->x[1] * (t->y[2] - t->y[0]) + 
+//         t->x[2] * (t->y[2] - t->y[3])
+//     );
+// }
+// void triangle_scale(triangle *t, float factor) {
+//     float cx = (t->x[0] + t->x[1] + t->x[2]) / 3.0f;
+//     float cy = (t->y[0] + t->y[1] + t->y[2]) / 3.0f;
+//     for(i32 i = 0; i < 3; i++) {
+//         t->x[i] = (t->x[i] - cx) * factor + cx;
+//         t->y[i] = (t->y[i] - cy) * factor + cy;
+//     }
+// }
+// float circle_area(circle *c) { return M_PI * c->radius * c->radius; }
+// void circle_scale(circle *c, float f) { c->radius *= f; }
+//
+// int main(int argc, char **argv) {
+//     rectangle r = { 0, 0, 30, 60 };
+//     float n = shape_area(&r->parent);
+//     shape_scale(&r->parent, 2.0f);
+//     assert((shape_area(&r->parent) - n * 4.0f) < 0.00001f);
+// }
+// --------------- EXPANDS TO --------------
+//
+// typedef struct shape {
+//   int type_id;
+// } shape;
+// typedef struct shape_vtbl {
+//   float (*area)(shape *self);
+//   void (*scale)(shape *self, float factor);
+// } shape_vtbl;
+// enum { TYPE_shape_rectangle, TYPE_shape_triangle, TYPE_shape_circle };
+// typedef struct rectangle rectangle;
+// typedef struct triangle triangle;
+// typedef struct circle circle;
+// float rectangle_area(rectangle *self);
+// void rectangle_scale(rectangle *self, float factor);
+// float triangle_area(triangle *self);
+// void triangle_scale(triangle *self, float factor);
+// float circle_area(circle *self);
+// void circle_scale(circle *self, float factor);
+// shape_vtbl shape_vtbls[] = {
+//     [TYPE_shape_rectangle] = {.area = (void *)rectangle_area,
+//                               .scale = (void *)rectangle_scale},
+//     [TYPE_shape_triangle] = {.area = (void *)triangle_area,
+//                              .scale = (void *)triangle_scale},
+//     [TYPE_shape_circle] = {.area = (void *)circle_area,
+//                            .scale = (void *)circle_scale}};
+// inline float shape_area(shape *self) {
+//   return shape_vtbls[self->type_id].area(self);
+// }
+// inline void shape_scale(shape *self, float factor) {
+//   return shape_vtbls[self->type_id].scale(self, factor);
+// }
+//
+#define interface(name, ...) \
     typedef struct name { int type_id; } name; \
     typedef struct name##_vtbl { \
-        name##_IFACE(INTERFACE_DECLARE) \
+        name##_IFACE(GEN_IFACE_VTBL) \
     } name##_vtbl; \
-    decom(APPLY_ALLm1(func2, name, name##_IFACE(func1) 0)) \
     enum { APPLY_ALL(GEN_ENUM, name, __VA_ARGS__) }; \
-    name##_vtbl name##_vtbls[];
-
-#define func0(...) (APPLY_ALL(keep_type, 0, __VA_ARGS__)), (APPLY_ALL(extract_var, 0, __VA_ARGS__))
-//#define func1(ret, name, self, ...) (ret, name, toss self, (keep self, APPLY_ALL(keep_type, 0, __VA_ARGS__)), (toss self, APPLY_ALL(extract_var, 0, __VA_ARGS__))),
-#define func1(ret, name, ...) (ret, name, toss decom1(__VA_ARGS__), func0(__VA_ARGS__)),
-#define collect(iface, ret, name, self, params, vars) inline ret iface##_##name params { return iface##_##vtbls[self->type_id].name vars; }
-#define collect1(arg) collect arg
-#define split_arg(...) __VA_ARGS__
-#define split_arg1(ctx, arg) (ctx, split_arg arg)
-#define func2(ctx, arg) collect1(split_arg1(ctx, arg))
-#define def2(name) decom(APPLY_ALLm1(func2, name, name##_IFACE(func1) 0))
-
-#define shape_IFACE(fn) \
-    fn(int, area, (shape*) self) \
-    fn(int, perim, (shape*) self)
-
-iface(shape, rect, triangle, circle);
-
-#define impl_func(ctx, var) .var = ctx##_##var,
-#define impl(iface, name) iface##_vtbl name##_vtbl = { APPLY_ALLm1(impl_func, name, iface##_IFACE(get_name) 0) }
-
-//#define def(iface, name) iface##_vtbl name##_vtbl = { APPLY_ALLm1(impl_func, name, iface##_IFACE(get_name) 0) }
-//def(shape, square);
+    DECOM(APPLY_ALL(GEN_TYPEDEFS, name, __VA_ARGS__)) \
+    DECOM(APPLY_ALL2(GEN_FUNC_DEC, name, __VA_ARGS__)) \
+    name##_vtbl name##_vtbls[] = { \
+        APPLY_ALL2(GEN_VTBLS, name, __VA_ARGS__) \
+    }; \
+    DECOM(APPLY_ALLm1(GEN_IFACE_FUNC, name, name##_IFACE(GEN_IFACE_FUNC_INFO) 0))
+#endif
